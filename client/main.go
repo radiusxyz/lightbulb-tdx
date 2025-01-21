81d81b32a24c4a1d2f08dea769f4474297e852b5
Wonjae Choi <wonjae@snu.ac.kr>
2025-01-21 20:22:38 +0900

    feat: Use enviroment variable at client

--- client/main.go
+++ client/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/radiusxyz/lightbulb-tdx/auction"
 )
 
 func main() {
+	// Load environment variables from .env file.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	// Create a new client instance.
-	client, err := auction.NewClient("localhost:50051")
+	client, err := auction.NewClient(os.Getenv("SERVER_ADDRESS") + ":" + os.Getenv("PORT"))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
